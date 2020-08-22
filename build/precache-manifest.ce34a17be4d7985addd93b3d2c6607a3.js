229e7d6fd5fce9ecd5afc8372d98362ecfe52219
Mahmoud <mhkazazz@hotmail.com>
2020-08-22 13:47:52 +1000

    build

---
+++ build/precache-manifest.ce34a17be4d7985addd93b3d2c6607a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adab6c051b143ad413523760db8fca1d",
+    "url": "/Mhkazzaz/random-quote/index.html"
+  },
+  {
+    "revision": "719bdddf05e0f075839b",
+    "url": "/Mhkazzaz/random-quote/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "e6ac2a693f509ec76590",
+    "url": "/Mhkazzaz/random-quote/static/css/main.728e3487.chunk.css"
+  },
+  {
+    "revision": "719bdddf05e0f075839b",
+    "url": "/Mhkazzaz/random-quote/static/js/2.a9bf3710.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Mhkazzaz/random-quote/static/js/2.a9bf3710.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6ac2a693f509ec76590",
+    "url": "/Mhkazzaz/random-quote/static/js/main.c9b081a8.chunk.js"
+  },
+  {
+    "revision": "68565edd1839ea40ecae",
+    "url": "/Mhkazzaz/random-quote/static/js/runtime-main.563a21ab.js"
+  }
+]);
